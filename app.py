a1c6bbbf716c4bcc79b0e55eea906a3766b55a14
Mrunal Tamhane <89576837+mrunaltamhane@users.noreply.github.com>
2023-06-02 02:29:29 +0530

    Update app.py
    minor changes in ui

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ kdramas.drop(236, axis=0, inplace=True)
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 st.title('KChingu')
 kdrama_selected = st.selectbox(
-    'How would you like to be contacted?', kdramas['Name'])
+    'Select the Kdrama you have watched', kdramas['Name'])
 
 if st.button('Recommend'):
     names,posters,homepage = recommend(kdrama_selected)
